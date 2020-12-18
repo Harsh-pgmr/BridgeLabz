326c93519d167639868088fc815a05540d812a17
Asur-harsh <hbb835@gmail.com>
2020-12-19 05:29:01 +0530

    Refactored:Prints the Hello world message and added header

--- com/bridgelabz/basicPrograms/hello_user.py
+++ com/bridgelabz/basicPrograms/hello_user.py
@@ -1,3 +1,12 @@
+"""
+ ******************************************************************************
+ *  Purpose: Prints the Hello world message
+ *
+ *  @author  Harhad B
+ *  @version 3.8
+ *  @since   17/12/2020
+ ******************************************************************************
+"""
 name = input("Enter User Name")  # Takes users input
 if len(name) > 3:  # Checks for input length
     print("Hello", name, "How are you")
