2d2ec2b57fd47cc2117c64bb3d17270da1419acb
Harsh <73747616+Harsh-pgmr@users.noreply.github.com>
2020-12-18 08:28:40 +0530

    Delete hello_user.py

--- com/bridgelabz/basicPrograms/hello_user.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter User Name")  # Takes users input
-if len(name) > 3:  # Checks for input length
-    print("Hello", name, "How are you")
-else:
-    print("User name should be minimum three characters")
