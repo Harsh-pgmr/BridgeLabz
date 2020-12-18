0e71afdb52fb881cdbd7d865c310d8181f340449
Asur-harsh <hbb835@gmail.com>
2020-12-18 14:59:14 +0530

    Added: Program to find prime numbers

---
+++ com/bridgelabz/basicPrograms/prime_number.py
@@ -0,0 +1,21 @@
+import math
+
+
+def primeFactors():
+    n = int(input("Enter a number to find prime factor"))
+    # no of even divisibility
+    while n % 2 == 0:
+        print(2),
+        n = n / 2
+    # n reduces to become odd
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        # while i divides n
+        while n % i == 0:
+            print(i)
+            n = n / i
+    # if n is a prime
+    if n > 2:
+        print(n)
+
+
+primeFactors()
