742d0c5df9952c667da8d4e0bc2ac1b0b8ac05ce
Asur-harsh <hbb835@gmail.com>
2020-12-19 08:07:55 +0530

    Refactored: prints the 2d array and matrix

--- com/bridgelabz/functionalPrograms/2d_Array.py
+++ com/bridgelabz/functionalPrograms/2d_Array.py
@@ -2,14 +2,18 @@ from numpy import *
 
 
 def two_dimensional_Array():
+    """
+
+    :rtype: matrix in 2d arrays
+    """
     while True:
         try:
             rows = int(input("Enter the number of rows:"))
             columns = int(input("Enter the number of columns:"))
             print("Enter the entries in a single line (separated by space): ")
-            entries = list(map(int, input().split()))
-            matrix = array(entries).reshape(rows, columns)
-            print(matrix)
+            entries = list(map(int, input().split()))  # User input of entries in a single line separated by space
+            matrix = array(entries).reshape(rows, columns)  # Logic
+            print(matrix)  # prints matrix
         except ValueError:
             print("Enter appropriate values matching Rows and columns ")
 
