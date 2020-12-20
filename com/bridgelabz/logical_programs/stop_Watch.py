8c44097b4ac46f7a96a9e924531cdc02d940baaf
Asur-harsh <hbb835@gmail.com>
2020-12-20 12:38:12 +0530

    Refactored : program to  measuring the time that elapses between the start and end clicks

--- com/bridgelabz/logical_programs/stop_Watch.py
+++ com/bridgelabz/logical_programs/stop_Watch.py
@@ -1,6 +1,6 @@
 """
  ******************************************************************************
- *  Purpose: To play gambler game
+ *  Purpose: Stopwatch Program for measuring the time that elapses between the start and end clicks
  *
  *  @author  Harshad B
  *  @version 3.8
@@ -17,9 +17,9 @@ class StopWatch:
             :parameter: None
             :return: This function will return elapsed time
         """
-        start = input("Press Enter to start timer: ")  # starts the timer
+        start = input("Press Enter a number  to start timer: ")  # starts the timer
         begin = time.time()
-        stop = input("Press Enter to stops timer: ")  # stops the timer
+        stop = input("Press Enter to a number stops timer: ")  # stops the timer
         end = time.time()  # gets the time when user press enter
         elapsed = end - begin  # difference between timer started and user press enter
         elapsed = round(elapsed, 2)
