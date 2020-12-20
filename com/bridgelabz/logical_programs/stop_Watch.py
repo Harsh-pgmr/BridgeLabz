652b438a657226536e64180122eee411f2ca1e8c
Asur-harsh <hbb835@gmail.com>
2020-12-20 12:07:26 +0530

    Added: files

---
+++ com/bridgelabz/logical_programs/stop_Watch.py
@@ -0,0 +1,39 @@
+"""
+ ******************************************************************************
+ *  Purpose: To play gambler game
+ *
+ *  @author  Harshad B
+ *  @version 3.8
+ *  @since   20/12/2020
+ ******************************************************************************
+"""
+import time
+
+
+class StopWatch:
+    def get_ElapsedTime(self):
+        """
+            Description:Simulates the stopwatch
+            :parameter: None
+            :return: This function will return elapsed time
+        """
+        start = input("Press Enter to start timer: ")  # starts the timer
+        begin = time.time()
+        stop = input("Press Enter to stops timer: ")  # stops the timer
+        end = time.time()  # gets the time when user press enter
+        elapsed = end - begin  # difference between timer started and user press enter
+        elapsed = round(elapsed, 2)
+
+        return elapsed
+
+
+if __name__ == '__main__':
+    while True:
+        try:                                # throws exception if occurs
+            timer = StopWatch()
+            elapse_Time = timer.get_ElapsedTime()     # called stopwatch function
+            print("Elapsed time is ", elapse_Time, "seconds")
+            break
+        except Exception:                       # catches exception if throws
+            print("enter valid number")
+            continue
